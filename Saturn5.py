d9a0d742a9a8050b5201542fbcdb8967d3379ace
Pedro Algarvio <pedro@algarvio.me>
2021-10-14 07:56:49 +0100

    Code Fixes to Saturn5 (#9)
    * Fix ``pre-commit`` issues
    
    * Don't forget to return the ``dataframe``
    
    * Rename ``Saturn5b`` to ``Saturn5``
    
    * Switch to flake8.
    
    Stop trying to install freqtrade, which in turn installs the problematic TA-Lib.

--- Saturn5.py
+++ Saturn5.py
@@ -11,7 +11,7 @@ def to_minutes(**timdelta_kwargs):
     return int(timedelta(**timdelta_kwargs).total_seconds() / 60)
 
 
-class Saturn5b(IStrategy):
+class Saturn5(IStrategy):
     timeframe = "15m"
 
     # Stoploss
@@ -132,6 +132,7 @@ class Saturn5b(IStrategy):
             dataframe["volume"] > 0,
         ]
         dataframe.loc[reduce(lambda x, y: x & y, s2_conditions), ["buy", "buy_tag"]] = (1, "buy_signal_2")
+        return dataframe
 
     def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         # This is essentailly ignored as we're using strict ROI / Stoploss / TTP sale scenarios
