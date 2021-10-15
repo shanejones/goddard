50e457dd66da2f423dc6f079165e8a2363f368f3
Pedro Algarvio <pedro@algarvio.me>
2021-10-15 18:05:39 +0100

    Sprinkle reports with some unicode

--- .github/workflows/scripts/comment-ci-results.py
+++ .github/workflows/scripts/comment-ci-results.py
@@ -56,93 +56,112 @@ def comment_results(options, results_data):
                 comment_body = f"# {exchange.capitalize()} - {currency.upper()} - {strategy.capitalize()}\n\n"
                 timeranges = results_data[exchange][name]["results"][currency][strategy]
                 for timerange in timeranges:
+
+                    def get_value_for_report(report_name, key, round_cases=0):
+                        value = results_data[exchange][report_name]["results"][currency][strategy][timerange][key]
+                        if not isinstance(value, str) and round_cases:
+                            value = round(value, round_cases)
+                        return value
+
                     comment_body += f"## {timerange}\n\n"
-                    report_table_header_1 = "| "
-                    report_table_header_2 = "| --: "
-                    for report_name in sorted_report_names:
-                        if report_name == "Current":
-                            report_table_header_1 += f"| {report_name} "
-                        else:
-                            report_sha = results_data[exchange][report_name]["sha"]
-                            report_table_header_1 += (
-                                f"| [{report_name}](https://github.com/{options.repo}/commit/{report_sha}) "
-                            )
-                        report_table_header_2 += "| --: "
-                    report_table_header_1 += "|\n"
-                    report_table_header_2 += "|\n"
-                    comment_body += report_table_header_1
-                    comment_body += report_table_header_2
+
+                    previous_report_sha = results_data[exchange]["Previous"]["sha"]
+                    previous_report_label = (
+                        f"[Previous](https://github.com/{options.repo}/commit/{previous_report_sha})"
+                    )
+                    comment_body += f"|     |      | Current | {previous_report_label} |\n"
+                    comment_body += "|  --: | :--: |     --: |                     --: |\n"
+
                     for key in sorted(timeranges[timerange]):
                         row_line = "| "
                         if key == "max_drawdown":
                             label = "Max Drawdown"
                             row_line += f" {label } |"
-                            for report_name in sorted_report_names:
-                                value = results_data[exchange][report_name]["results"][currency][strategy][timerange][
-                                    key
-                                ]
-                                if not isinstance(value, str):
-                                    value = f"{round(value, 4)} %"
-                                row_line += f" {value} |"
+                            current_value = get_value_for_report("Current", key, round_cases=4)
+                            previous_value = get_value_for_report("Previous", key, round_cases=4)
+                            if current_value > previous_value:
+                                row_line += f" \N{DOUBLE EXCLAMATION MARK} | {current_value} % |"
+                            elif current_value == previous_value:
+                                row_line += f" \N{SNOWFLAKE} | {current_value} % |"
+                            elif current_value < previous_value:
+                                row_line += f" \N{ROCKET} | {current_value} % |"
+                            row_line += f" {previous_value} % |"
                             comment_body += f"{row_line}\n"
                         elif key == "profit_mean_pct":
                             label = "Profit Mean"
                             row_line += f" {label } |"
-                            for report_name in sorted_report_names:
-                                value = results_data[exchange][report_name]["results"][currency][strategy][timerange][
-                                    key
-                                ]
-                                if not isinstance(value, str):
-                                    value = f"{round(value, 4)} %"
-                                row_line += f" {value} |"
+                            current_value = get_value_for_report("Current", key, round_cases=4)
+                            previous_value = get_value_for_report("Previous", key, round_cases=4)
+                            if current_value > previous_value:
+                                row_line += f" \N{ROCKET} | {current_value} % |"
+                            elif current_value == previous_value:
+                                row_line += f" \N{SNOWFLAKE} | {current_value} % |"
+                            elif current_value < previous_value:
+                                row_line += f" \N{DOUBLE EXCLAMATION MARK} | {current_value} % |"
+                            row_line += f" {previous_value} % |"
                             comment_body += f"{row_line}\n"
                         elif key == "profit_sum_pct":
                             label = "Profit Sum"
+                            current_value = get_value_for_report("Current", key, round_cases=4)
+                            previous_value = get_value_for_report("Previous", key, round_cases=4)
                             row_line += f" {label } |"
-                            for report_name in sorted_report_names:
-                                value = results_data[exchange][report_name]["results"][currency][strategy][timerange][
-                                    key
-                                ]
-                                if not isinstance(value, str):
-                                    value = f"{round(value, 4)} %"
-                                row_line += f" {value} |"
+                            if current_value > previous_value:
+                                row_line += f" \N{ROCKET} | {current_value} % |"
+                            elif current_value == previous_value:
+                                row_line += f" \N{SNOWFLAKE} | {current_value} % |"
+                            elif current_value < previous_value:
+                                row_line += f" \N{DOUBLE EXCLAMATION MARK} | {current_value} % |"
+                            row_line += f" {previous_value} % |"
                             comment_body += f"{row_line}\n"
                         elif key == "profit_total_pct":
                             label = "Profit Total"
+                            current_value = get_value_for_report("Current", key, round_cases=4)
+                            previous_value = get_value_for_report("Previous", key, round_cases=4)
                             row_line += f" {label } |"
-                            for report_name in sorted_report_names:
-                                value = results_data[exchange][report_name]["results"][currency][strategy][timerange][
-                                    key
-                                ]
-                                if not isinstance(value, str):
-                                    value = f"{round(value, 4)} %"
-                                row_line += f" {value} |"
+                            if current_value > previous_value:
+                                row_line += f" \N{ROCKET} | {current_value} % |"
+                            elif current_value == previous_value:
+                                row_line += f" \N{SNOWFLAKE} | {current_value} % |"
+                            elif current_value < previous_value:
+                                row_line += f" \N{DOUBLE EXCLAMATION MARK} | {current_value} % |"
+                            row_line += f" {previous_value} % |"
                             comment_body += f"{row_line}\n"
                         elif key == "winrate":
                             label = "Win Rate"
+                            current_value = get_value_for_report("Current", key, round_cases=4)
+                            previous_value = get_value_for_report("Previous", key, round_cases=4)
                             row_line += f" {label } |"
-                            for report_name in sorted_report_names:
-                                value = results_data[exchange][report_name]["results"][currency][strategy][timerange][
-                                    key
-                                ]
-                                if not isinstance(value, str):
-                                    value = f"{round(value, 4)} %"
-                                row_line += f" {value} |"
+                            if current_value > previous_value:
+                                row_line += f" \N{ROCKET} | {current_value} % |"
+                            elif current_value == previous_value:
+                                row_line += f" \N{SNOWFLAKE} | {current_value} % |"
+                            elif current_value < previous_value:
+                                row_line += f" \N{DOUBLE EXCLAMATION MARK} | {current_value} % |"
+                            row_line += f" {previous_value} % |"
                             comment_body += f"{row_line}\n"
+                        elif key == "trades":
+                            label = "Trades"
+                            current_value = get_value_for_report("Current", key)
+                            previous_value = get_value_for_report("Previous", key)
+                            row_line += f" {label } |"
+                            if current_value > previous_value:
+                                row_line += f" \N{ROCKET} | {current_value} |"
+                            elif current_value == previous_value:
+                                row_line += f" \N{SNOWFLAKE} | {current_value} |"
+                            elif current_value < previous_value:
+                                row_line += f" \N{DOUBLE EXCLAMATION MARK} | {current_value} |"
+                            row_line += f" {previous_value} |"
+                            comment_body += f"{row_line}\n"
+                        elif key == "duration_avg":
+                            current_value = get_value_for_report("Current", key)
+                            previous_value = get_value_for_report("Previous", key)
+                            label = "Average Duration"
+                            comment_body += f" {label } | \N{STOPWATCH} | {current_value} | {previous_value} |\n"
                         else:
-                            if key == "duration_avg":
-                                label = "Average Duration"
-                            elif key == "trades":
-                                label = "Trades"
-                            else:
-                                label = key
-                            row_line += f" {label } |"
-                            for report_name in sorted_report_names:
-                                value = results_data[exchange][report_name]["results"][currency][strategy][timerange][
-                                    key
-                                ]
-                                row_line += f" {value} |"
-                            comment_body += f"{row_line}\n"
+                            current_value = get_value_for_report("Current", key)
+                            previous_value = get_value_for_report("Previous", key)
+                            label = key
+                            comment_body += f" {label } | | {current_value} | {previous_value} |\n"
                     ft_output = (
                         options.path / "current" / exchange / currency / strategy / f"backtest-output-{timerange}.txt"
                     )
